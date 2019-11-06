1d9c4170cfdb56d2186ac6b9614367fa18d33b66
Durgesh2398 <2308durgesh@gmail.com>
2019-11-07 00:15:28 +0530

    task4

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,61 +1,72 @@
 public class BinarySearchTree {
-class Node
-{
-int data;
-Node left,right;
-
-public Node(int data)
-{
-this.data=data;
-left=right=null;
-}
-}
-Node root;
-    BinarySearchTree() {  
-        root = null;  
-    }
-   
-    void insert(int data)
+    class Node
     {
-     
-   
-    }
-    void inorder()
+    int data;
+    Node left,right;
+    
+    public Node(int data)
     {
-      inorder1(root);
+    this.data=data;
+    left=right=null;
     }
-    public static void inorder1(Node root)
-   {
-     if(root!=null)
-       {
-           inorder1(root.left);
-            System.out.println(root.data);
-            inorder1(root.right);
-       }
-  }
-
-   public void preorder()
-    {
-   
     }
-   public void postorder()
-    {
-   
-    }
-    public static void main(String args[])
-    {
-    BinarySearchTree b=new BinarySearchTree();
-    b.insert(60);
-        b.insert(20);
-        b.insert(10);
-        b.insert(40);
-        b.insert(70);
-        b.insert(60);
-        b.insert(80);
-        b.inorder();
-        b.preorder();
-        b.postorder();
+    Node root;
+        BinarySearchTree() {  
+            root = null;  
+        }
        
-    }
+        void insert(int data)
+        {
+         
+       
+        }
+        void inorder()
+        {
+          inorder1(root);
+        }
+        public static void inorder1(Node root)
+       {
+         if(root!=null)
+           {
+               inorder1(root.left);
+                System.out.println(root.data);
+                inorder1(root.right);
+           }
+      }
+    
+       public void preorder()
+        {
 
-}
+        }
+
+       public void postorder()
+        {
+            postorder1(root);
+        }
+        public static void postorder1(Node root)
+        {
+            if(root!=null)
+            {
+                postorder1(root.left);
+                postorder1(root.right);
+                System.out.println(root.data+ " ");
+            }
+        }
+        public static void main(String args[])
+        {
+        BinarySearchTree b=new BinarySearchTree();
+            b.insert(60);
+            b.insert(20);
+            b.insert(10);
+            b.insert(40);
+            b.insert(70);
+            b.insert(60);
+            b.insert(80);
+            b.inorder();
+            b.preorder();
+            b.postorder();
+           
+
+        }
+    
+    }
